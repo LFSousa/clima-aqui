28f1ac4fe89acb64a31d1d16e9b2a2bf47f228af
Luis Filipe <luis@filipe.xyz>
2020-05-14 18:28:18 -0400

    remove the icon theme provider

--- src/index.js
+++ src/index.js
@@ -4,20 +4,8 @@ import "./styles.scss";
 import Main from "./views/Main";
 import moment from "moment";
 import "moment/locale/pt-br";
-import { WeatherThemeProvider } from "weather-styled-icon";
 
 // Define a linguagem padrão do moment como português
 moment.locale("pt-br");
 
-// Define o tema dos icones de clima
-const theme = {
-  sunColor: "Orange",
-  raysColor: "OrangeRed",
-};
-
-ReactDOM.render(
-  <WeatherThemeProvider theme={theme}>
-    <Main />
-  </WeatherThemeProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Main />, document.getElementById("root"));
